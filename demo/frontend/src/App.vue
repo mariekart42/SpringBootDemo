7d8a83a59934cdfb946769aef4cb8d2276f5baba
mariekart42 <marie.a.mensing@gmail.com>
2025-04-30 15:15:33 +0200

    testing to use enpoints from backend

--- demo/frontend/src/App.vue
+++ demo/frontend/src/App.vue
@@ -1,16 +1,35 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const message = ref('')
+const fullInfo = ref('')
+const userName = ref('')
 
-onMounted(async () => {
-  const res = await fetch('http://localhost:9090/api/hello')
-  message.value = await res.text()
-})
+// onMounted(async () => {
+//   const res = await fetch('http://localhost:9090/api/test/69')
+//   //message.value = await res.text()
+// })
+
+async function getFullInfo() {
+
+  if (userName.value === '')
+    return fullInfo.value = "Common provide your name";
+  const res = await fetch('http://localhost:9090/api/getFullInfo/' + userName.value)
+  fullInfo.value = await res.text()
+
+  // const res = await fetch('http://localhost:9090/api/setProfile', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ name: userName.value })
+  // })
+  // message.value = await res.text()
+  // alert(text)
+}
 </script>
 
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <h1>{{ fullInfo }}</h1>
+    <input v-model="userName" placeholder="Dein Name" />
+    <button @click="getFullInfo">Senden</button>
   </div>
 </template>
